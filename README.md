a8f78c03062de5cc2ee5ed624681e20fa1a3dabc
kmohenski <karlo.mohenski@gmail.com>
2024-07-23 00:18:00 +0200

    Adding token

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# classifier_demo
+# Demo
 
-Quick classifier demo for classifier.
+[Check the app here](https://classifier-demo.streamlit.app/)
+
+App that classifies the sentences as obligations, lawmaker obligations or not obligations.
+
+If the sentance is an obligation then the app runs 2 more classifiers.
+
+First is a category classifier that puts the Obligation into one of several categories.
+
+Second is an activity classifier that tells us what activities are required in the Obligation.
--- obligation_classifier.py
+++ obligation_classifier.py
@@ -1,10 +1,14 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
+
+import streamlit as st
 import torch
+
+hf_token = st.secrets["huggingface_token"]
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-tokenizer = AutoTokenizer.from_pretrained("lejzi/obligation_classifier")
-loaded = AutoModelForSequenceClassification.from_pretrained("lejzi/obligation_classifier")
+tokenizer = AutoTokenizer.from_pretrained("lejzi/activity_classifier", hf_token)
+loaded = AutoModelForSequenceClassification.from_pretrained("lejzi/activity_classifier", hf_token)
 loaded.to(device)
 
 pipe = TextClassificationPipeline(model=loaded, tokenizer=tokenizer, return_all_scores=True)
